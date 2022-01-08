dc5855d5f0be8d9b1e9da37d9403a55ab44deaf8
kunwu <wk707060335@gmail.com>
2022-01-08 21:58:57 +0100

    add telemetry data

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use std::net::TcpListener;
 
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello Rust");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
@@ -14,5 +16,7 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use actix_web::dev:: Server;
 use actix_web:: {web, App, HttpServer};
+use tracing_actix_web::TracingLogger;
 use sqlx::PgPool;
 use std::net:: TcpListener;
 
@@ -15,6 +16,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer:: new(move || {
             App:: new()
+                .wrap(TracingLogger::default())
                 .route("/health_check", web::get().to(health_check))
                 .route("/subscriptions", web::post().to(subscribe))
                 .app_data(db_pool.clone())
